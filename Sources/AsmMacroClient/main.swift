53e815ccfb51a7d5f8e823f0e4710455b1de5e4f
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-05-31 17:59:11 +0900

    Add example codes

--- Sources/AsmMacroClient/main.swift
+++ Sources/AsmMacroClient/main.swift
@@ -1,6 +1,8 @@
 import AsmMacro
 
 #if compiler(>=6.3)
+
+// MARK: - add
 @Asm(
     """
     add x0, x0, x1
@@ -9,8 +11,31 @@ import AsmMacro
     arch: .arm64
 )
 func add(_ lhs: UInt64, _ rhs: UInt64) -> UInt64
+print("The generated answer is \(add(13, 32))")
 
-print("The generated answer is \(add(10, 32))")
+
+// MARK: - function call
+
+@Asm(
+    """
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+    blr x1
+    ldp x29, x30, [sp], #16
+    ret
+    """
+)
+func asm_functionCall(string: UnsafePointer<CChar>, print: UnsafeRawPointer)
+
+var myPrint: (@convention(c) (UnsafePointer<CChar>) -> Void) = { ptr in
+    print(String(cString: ptr))
+}
+
+"hello".withCString { char in
+    let raw = unsafeBitCast(myPrint, to: UnsafeRawPointer.self)
+    asm_functionCall(string: char, print: raw)
+}
+
 #else
 print("AsmMacroClient requires Swift 6.3 or newer.")
 #endif
